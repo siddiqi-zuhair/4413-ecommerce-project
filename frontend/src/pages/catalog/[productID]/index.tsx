57d0d6820eb5c4ba197ea9d33bcc9ab132d47bdc
Zuhair Siddiqi <58523211+siddiqi-zuhair@users.noreply.github.com>
2024-07-23 22:55:22 -0400

    Update index.tsx

--- frontend/src/pages/catalog/[productID]/index.tsx
+++ frontend/src/pages/catalog/[productID]/index.tsx
@@ -1,15 +1,7 @@
+import { Item } from "../../../interfaces/item";
 
-export default function Product(){ 
+export default function Product({ item }: { item: Item }) { 
  
-      const mario: Item = {
-        name: "Mario",
-        price: 60,
-        quantity: 10,
-        description: "Mario is a platform game",
-        platform: "Nintendo",
-        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1mxf.webp",
-        video: "https://www.youtube.com/embed/5kcdRBHM7kM",
-      };
 
     return (
         <div className="bg-stone-50 flex flex-col items-center justify-start w-full h-full text-gray-600 ">
