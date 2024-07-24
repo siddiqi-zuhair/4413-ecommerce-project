82f5d6598ef01cf06a77aaa264d58bf9f1876cac
Zuhair Siddiqi <58523211+siddiqi-zuhair@users.noreply.github.com>
2024-07-23 22:58:53 -0400

    Update index.tsx

--- frontend/src/pages/catalog/[productID]/index.tsx
+++ frontend/src/pages/catalog/[productID]/index.tsx
@@ -1,11 +1,28 @@
 import { Item } from "../../../interfaces/item";
 
-export default function Product({ item }: { item: Item }) { 
- 
+export default function Product() { 
+    const mario: Item   = {
+        id: 1,
+        name: "Mario",
+        price: 60,
+        quantity: 10,
+        description: "Mario is a platform game",
+        platform: "Nintendo",
+        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1mxf.webp",
+        video: "https://www.youtube.com/embed/5kcdRBHM7kM"
+      }
 
     return (
-        <div className="bg-stone-50 flex flex-col items-center justify-start w-full h-full text-gray-600 ">
-            <p className="text-8xl font-black tracking-wider">Mario</p>
+        <div className="bg-stone-50 flex flex-col items-start justify-start w-full p-20 h-full text-gray-600  min-h-screen">
+            <p className="text-6xl font-black tracking-wider">{mario.name}</p>
+            <p className="text-4xl">{mario.description}</p>
+            <p className="text-4xl">{mario.platform}</p>
+            <p className="text-4xl">${mario.price}</p>
+            <p className="text-4xl">{mario.quantity}</p>
+            <div className="aspect-square">
+                <img className="rounded-xl" src={mario.image} alt={mario.name} />
+            </div>
+            <iframe width="560" height="315" src={mario.video}></iframe>
         </div>
     )
 
