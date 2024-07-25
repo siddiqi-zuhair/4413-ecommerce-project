e98e35fdbc258ed5f5a153a3f27f273a00384b10
Zuhair Siddiqi <58523211+siddiqi-zuhair@users.noreply.github.com>
2024-07-24 22:50:11 -0400

    changed sign in routes

--- frontend/src/pages/catalog/[productID]/index.tsx
+++ frontend/src/pages/catalog/[productID]/index.tsx
@@ -1,29 +1,29 @@
 import { Item } from "../../../interfaces/item";
 
-export default function Product() { 
-    const mario: Item   = {
-        id: 1,
-        name: "Mario",
-        price: 60,
-        quantity: 10,
-        description: "Mario is a platform game",
-        platform: "Nintendo",
-        image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1mxf.webp",
-        video: "https://www.youtube.com/embed/5kcdRBHM7kM"
-      }
+export default function Product() {
+  const mario: Item = {
+    id: 1,
+    name: "Super Mario Odyssey",
+    price: 60,
+    quantity: 10,
+    description: "Mario is a platform game",
+    platform: "Nintendo Switch",
+    image: "https://images.igdb.com/igdb/image/upload/t_cover_big/co1mxf.webp",
+    video: "https://www.youtube.com/embed/5kcdRBHM7kM",
+  };
 
-    return (
-        <div className="bg-stone-50 flex flex-col items-start justify-start w-full p-20 h-full text-gray-600  min-h-screen">
-            <p className="text-6xl font-black tracking-wider">{mario.name}</p>
-            <p className="text-4xl">{mario.description}</p>
-            <p className="text-4xl">{mario.platform}</p>
-            <p className="text-4xl">${mario.price}</p>
-            <p className="text-4xl">{mario.quantity}</p>
-            <div className="aspect-square">
-                <img className="rounded-xl" src={mario.image} alt={mario.name} />
-            </div>
-            <iframe width="560" height="315" src={mario.video}></iframe>
-        </div>
-    )
-
+  return (
+    <div className="bg-stone-50 flex flex-col items-start justify-start w-full p-20 h-full text-gray-600  min-h-screen">
+      <div className="flex text-5xl font-black tracking-wider items-center justify-center ">
+        <span className="border-r-2 border-gray-600 pr-2">{mario.name}</span> <span className="text-2xl pl-2"> {mario.platform}</span>{" "}
+      </div>
+      <p className="text-4xl">{mario.description}</p>
+      <p className="text-4xl">${mario.price}</p>
+      <p className="text-4xl">{mario.quantity}</p>
+      <div className="aspect-square">
+        <img className="rounded-xl" src={mario.image} alt={mario.name} />
+      </div>
+      <iframe width="560" height="315" src={mario.video}></iframe>
+    </div>
+  );
 }
