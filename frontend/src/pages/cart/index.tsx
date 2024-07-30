49c98a4135ca595836f9c3acc45ed3a11ea0de4a
Zuhair Siddiqi <58523211+siddiqi-zuhair@users.noreply.github.com>
2024-07-30 19:47:16 -0400

    Update index.tsx

--- frontend/src/pages/cart/index.tsx
+++ frontend/src/pages/cart/index.tsx
@@ -124,7 +124,7 @@ export default function Cart() {
             <p className="text-xl font-semibold">
               ${calculateTotal().toFixed(2)}
             </p>
-            <button className="bg-red-500 text-white px-4 py-2 rounded-lg">
+            <button disabled className="bg-red-500 text-white px-4 py-2 rounded-lg disabled:contrast-50 disabled:hover:cursor-not-allowed">
               Checkout
             </button>
           </div>
