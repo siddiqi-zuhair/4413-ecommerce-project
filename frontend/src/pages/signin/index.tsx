83b437e761be939ff33595c7341d27a8fd74cba6
Zuhair Siddiqi <58523211+siddiqi-zuhair@users.noreply.github.com>
2024-08-17 22:52:12 -0400

    Updated sign in styles

--- frontend/src/pages/signin/index.tsx
+++ frontend/src/pages/signin/index.tsx
@@ -23,7 +23,6 @@ export default function SignIn() {
       if (response.ok) {
         const data = await response.json();
         login(data.token);
-        // Add cart to user's account if there are items in localStorage
         if (redirect !== undefined) {
           router.push(`/${redirect}`);
         } else {
@@ -43,9 +42,9 @@ export default function SignIn() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 text-gray-600">
+    <div className="flex items-center justify-center min-h-[calc(100vh-112px)] bg-gray-100 text-gray-600">
       <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-3xl font-semibold mb-6 text-center">Sign in</h2>
+        <h2 className="text-5xl font-bold mb-6 text-center">Sign In</h2>
         <form onSubmit={handleSubmit} className="space-y-4">
           <div>
             <input
@@ -55,7 +54,7 @@ export default function SignIn() {
               value={username}
               onChange={(e) => setUsername(e.target.value)}
               required
-              className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <div>
@@ -66,20 +65,23 @@ export default function SignIn() {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
-              className="w-full px-4 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
           <button
             type="submit"
-            className="w-full py-2 bg-blue-500 text-white rounded hover:bg-blue-700"
+            className="w-full py-2 bg-red-500 text-white rounded-xl outline outline-1 hover:bg-white font-bold hover:text-black"
           >
             Sign In
           </button>
         </form>
-        <div className="flex justify-between items-center mt-4">
-          <button onClick={handleSignUp} className="text-blue-500">
-            Create account
-          </button>
+        <div className="text-center text-sm mt-4 text-gray-600">
+          <p>
+            Don't have an account?{" "}
+            <a href="#" onClick={handleSignUp} className="text-blue-500">
+              Create one.
+            </a>
+          </p>
         </div>
       </div>
     </div>
