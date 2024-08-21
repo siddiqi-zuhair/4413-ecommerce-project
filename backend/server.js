44eb9cb8fc6036e2e3ca960adf35d6f21a500a8d
Zuhair Siddiqi <58523211+siddiqi-zuhair@users.noreply.github.com>
2024-08-21 17:15:14 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: "*",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
